374066d5e3b4e1a21082dffb380be24af09da05d
BulliDZ <58221415+BulliDZ@users.noreply.github.com>
2021-06-15 17:19:15 +0300

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 
 class TestStore:
@@ -6,6 +7,8 @@ class TestStore:
         browser.implicitly_wait(10)
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
+        time.sleep(30)
         print("In browser...")
         basket_button = browser.find_element_by_css_selector(".btn.btn-add-to-basket")
         print(basket_button.text.lower())
+        assert basket_button
